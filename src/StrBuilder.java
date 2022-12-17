3ab70e433760e1d4db316faf0cb274277aeee0ad
Wruthless <wsgnzls@comcast.net>
2022-12-17 10:14:38 -0500

    Added a StringBuilder example.

---
+++ src/StrBuilder.java
@@ -0,0 +1,19 @@
+/*
+    String is immutable in Java, whenever String manipulation occurs a new String is generated
+    and the old one, Java discards. These String operations are expensive and generate a heavy
+    workload for the garbage collector. StringBuilder (and StringBuffer) are mutable objects,
+    eliminating the discarding of cold.
+ */
+public class StrBuilder {
+
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Lorem ");
+        stringBuilder.append("Ipsum ");
+        stringBuilder.append("Dolor ");
+        stringBuilder.append("Nurit");
+
+        System.out.println(stringBuilder.toString());
+    }
+
+}
